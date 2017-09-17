5ef507bb65560f775253974b38956bb6336d0749
Alex Fore <forealexander4@gmail.com>
2017-09-17 13:59:56 -0500

    hover function. change num of squares.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
 $(document).ready(function() {
-  for (var i = 0; i < 16; i++) {
+  var squares = 16
+  // 16 rows with 16 divs
+  for (var i = 0; i < squares; i++) {
     var row = $('<div class="grid-rows"></div>')
     row.appendTo('#container')
-    for (var b = 0; b < 16; b++) {
+    for (var b = 0; b < squares; b++) {
       var grid = $("<div class='grid-unit'></div>");
       grid.appendTo('.grid-rows:last-child');
     }
   }
+
+  // changes squares to dark grey on mouse enter
+  darkGrey = $('.grid-unit').on('mouseenter', function() {
+    $(this).addClass('dark-grey');
+  });
+
+  // Prompt button
+  $('#clr-btn').on('click', function() {
+    var userInput = prompt('How many squares per side?');
+    var squares = parseInt(userInput);
+    $('.grid-rows').remove();
+    for (var i = 0; i < squares; i++) {
+      var row = $('<div class="grid-rows"></div>')
+      row.appendTo('#container')
+      for (var b = 0; b < squares; b++) {
+        var grid = $("<div class='grid-unit'></div>");
+        grid.appendTo('.grid-rows:last-child');
+      }
+    }
+
+    $('.grid-unit').on('mouseenter', function() {
+      $(this).addClass('dark-grey');
+    });
+
+  });
 });
